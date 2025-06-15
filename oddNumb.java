e964a437121f18c474eff5ceacc760d9fae4dc41
Manonsha <122508073+Manonsha@users.noreply.github.com>
2025-06-15 16:07:29 +0530

    Add files via upload

---
+++ oddNumb.java
@@ -0,0 +1,19 @@
+package projectUpload;
+
+public class oddNumb {
+	
+	public static void main(String [] args) {
+	
+		int a=8;
+		 int b=8;
+		 int c=1;
+	
+		 if(a==b) {
+		 	System.out.println(c);
+		 }else if(b==c) {
+		 	System.out.println(a);
+		 }else {
+		 	System.out.println(b);
+		 }	
+}
+}
